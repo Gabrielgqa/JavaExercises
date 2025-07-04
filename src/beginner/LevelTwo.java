053c18b087042717fcedfa691c3002a76914aa0d
GabrielGQA <gabrielgqa@gmail.com>
2025-07-04 07:04:29 -0300

    feat: iniciante salario

--- src/beginner/LevelTwo.java
+++ src/beginner/LevelTwo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class LevelTwo {
     public static void main(String[] args){
-        media1();
+        salario();
     }
 
     public static void media1(){
@@ -18,5 +18,17 @@ public class LevelTwo {
         double media = (A * 3.5 + B * 7.5) / 11;
 
         System.out.printf("MEDIA = %.5f",media);
+    }
+
+    public static void salario(){
+        int id, numeroHoras;
+        double valorHora;
+        Scanner sc = new Scanner(System.in);
+        id = sc.nextInt();
+        numeroHoras = sc.nextInt();
+        valorHora = sc.nextDouble();
+
+        System.out.println("NUMBER = "+id);
+        System.out.printf("SALARY = U$ %.2f", numeroHoras * valorHora);
     }
 }
