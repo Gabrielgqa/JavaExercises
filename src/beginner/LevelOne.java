cee3eee0c9728987ac57bcdf2fb4482862bc50b2
GabrielGQA <gabrielgqa@gmail.com>
2025-07-04 06:23:59 -0300

    feat: iniciante intervalo2

--- src/beginner/LevelOne.java
+++ src/beginner/LevelOne.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class LevelOne {
     public static void main(String [] args){
-        seisNumerosImpares();
+        intervalo2();
     }
 
     public static void somaSimples(){
@@ -224,5 +224,24 @@ public class LevelOne {
              }
              System.out.println(x);
          }
+    }
+
+    public static void intervalo2(){
+        int n,valor, somaDentro = 0, somaFora = 0;
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            valor = sc.nextInt();
+            if (valor >= 10 && valor <= 20)
+                somaDentro++;
+            else
+                somaFora++;
+
+        }
+
+        System.out.println(somaDentro + " in");
+        System.out.println(somaFora + " out");
     }
 }
